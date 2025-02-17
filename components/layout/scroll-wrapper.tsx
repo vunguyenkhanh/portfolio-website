0f662181b896e28851e45be00babe5d356d46aef
vunguyenkhanh <vunguyenkhanh.6599@gmail.com>
2025-02-17 15:20:22 +0700

    refactor: Improve OverlayScrollbars configuration and styling

--- components/layout/scroll-wrapper.tsx
+++ components/layout/scroll-wrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Options } from 'overlayscrollbars';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import 'overlayscrollbars/overlayscrollbars.css';
 import { ReactNode } from 'react';
@@ -8,21 +9,54 @@ interface ScrollWrapperProps {
   children: ReactNode;
 }
 
-const options = {
+const options: Options = {
   scrollbars: {
     theme: 'os-theme-dark',
+    visibility: 'auto',
     autoHide: 'scroll',
     autoHideDelay: 400,
+    autoHideSuspend: false,
+    dragScroll: true,
+    clickScroll: true,
+    pointers: ['mouse', 'touch', 'pen'],
   },
   overflow: {
     x: 'hidden',
+    y: 'scroll',
+  },
+  paddingAbsolute: false,
+  showNativeOverlaidScrollbars: false,
+  update: {
+    elementEvents: [['img', 'load']],
+    debounce: [0, 33],
+    attributes: null,
+    ignoreMutation: null,
   },
 };
 
 export function ScrollWrapper({ children }: ScrollWrapperProps) {
+  const handleInitialized = () => {
+    document.documentElement.classList.add('os-initialized');
+  };
+
+  const handleDestroyed = () => {
+    document.documentElement.classList.remove('os-initialized');
+  };
+
   return (
-    <OverlayScrollbarsComponent options={options} defer className="h-screen">
-      {children}
-    </OverlayScrollbarsComponent>
+    <div className="relative h-screen w-full overflow-hidden">
+      <OverlayScrollbarsComponent
+        options={options}
+        defer
+        element="div"
+        className="absolute inset-0"
+        events={{
+          initialized: handleInitialized,
+          destroyed: handleDestroyed,
+        }}
+      >
+        {children}
+      </OverlayScrollbarsComponent>
+    </div>
   );
 }
